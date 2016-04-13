5258785cdff87566037488a8a11be033551a0cf0
xyzdudu <68288504@qq.com>
2016-04-13 15:35:53 +0800

    增加界面

--- BtcClient.py
+++ BtcClient.py
@@ -1,63 +1,88 @@
 # -*- coding: utf-8 -*-
+from tkinter import *
 from BtcSpotAPI import BtcSpot
 from Datatable import draw
 from FiexdInvestment import fiexdInvestment
 import json
 import time
 
+class BtcClient(Frame):
+	#初始化apikey，secretkey,url
+	apikey = ''
+	secretkey = ''
+	okcoinRESTURL = 'www.okcoin.cn'
 
-#初始化apikey，secretkey,url
-apikey = ''
-secretkey = ''
-okcoinRESTURL = 'www.okcoin.cn'
+	#现货API
+	okcoinSpot = BtcSpot(okcoinRESTURL,apikey,secretkey)
 
-#现货API
-okcoinSpot = BtcSpot(okcoinRESTURL,apikey,secretkey)
+	money = 2000
+	coin = 0
 
-money = 2000
-coin = 0
+	def __init__(self, master=None):
+		Frame.__init__(self,master)
+		self.pack()
+		self.createWidgets()
 
-def klineData():
-	arrkline = okcoinSpot.kline('btc_cny','1min','1','1417536000000')
-	oneKline = arrkline[0]
-	openPrice = oneKline[1]
-	upPrice = oneKline[2]
-	downPrice = oneKline[3]
-	overPrice = oneKline[4]
-	print('开：' + '%s'%openPrice + '	高：' + '%s'%upPrice + '	低：' + '%s'%downPrice + '	收：' + '%s'%overPrice)
-	return oneKline
+	def createWidgets(self):
+		self.startButton = Button(self, text='开始', width='10', command=self.onStart)
+		self.startButton.grid(row=0, column=0)
+		self.stopButton = Button(self, text='结束', width='10')
+		self.stopButton.grid(row=0, column=1)
+		self.oneKeyBuyButton = Button(self, text='一键下单', width='10')
+		self.oneKeyBuyButton.grid(row=1, column=0)
+		self.oneKeySaleButton = Button(self, text='一键平仓', width='10')
+		self.oneKeySaleButton.grid(row=1, column=1)
 
-def arrKlineData(size = '1'):
-	return okcoinSpot.kline('btc_cny','15min',size,'1417536000000')
-
-if __name__ == '__main__':
-	# draw()
-	arrData = arrKlineData('2880')
-	price0 = []
-	labels = []
-	data = []
-	overPrice = 0
-
-	index = 0
-	for oneKline in arrData:
+	def klineData():
+		arrkline = okcoinSpot.kline('btc_cny','1min','1','1417536000000')
+		oneKline = arrkline[0]
+		openPrice = oneKline[1]
+		upPrice = oneKline[2]
+		downPrice = oneKline[3]
 		overPrice = oneKline[4]
-		tradingvolume = oneKline[5]
-		timeStamp = oneKline[0]
+		print('开：' + '%s'%openPrice + '	高：' + '%s'%upPrice + '	低：' + '%s'%downPrice + '	收：' + '%s'%overPrice)
+		return oneKline
 
-		arrTime = time.localtime(timeStamp / 1000)
-		styleTime = time.strftime("%d",arrTime)
-		index = index + 1
+	def arrKlineData(self, size = '1'):
+		return self.okcoinSpot.kline('btc_cny','15min',size,'1417536000000')
 
-		price0.append(overPrice)
-		labels.append('%s'%index)
-		data.append({'price':overPrice,'volume':tradingvolume})
+	def onStart(self):
+		arrData = self.arrKlineData('2880')
+		price0 = []
+		labels = []
+		data = []
+		overPrice = 0
 
-	draw(price0,labels)
-	btcData = fiexdInvestment(money, coin, data)
-	money = btcData['money']
-	coin = btcData['coin']
-	print("总价值：" + '%.2f'%(money + coin * overPrice))
-		
+		index = 0
+		for oneKline in arrData:
+			overPrice = oneKline[4]
+			tradingvolume = oneKline[5]
+			timeStamp = oneKline[0]
+
+			arrTime = time.localtime(timeStamp / 1000)
+			styleTime = time.strftime("%d",arrTime)
+			index = index + 1
+
+			price0.append(overPrice)
+			labels.append('%s'%index)
+			data.append({'price':overPrice,'volume':tradingvolume})
+
+		draw(price0,labels)
+		btcData = fiexdInvestment(self.money, self.coin, data)
+		self.money = btcData['money']
+		self.coin = btcData['coin']
+		print("总价值：" + '%.2f'%(self.money + self.coin * overPrice))
+
+		self.totalPrice = Label(self,text="总价值：" + '%.2f'%(self.money + self.coin * overPrice))
+		self.totalPrice.grid(row=2, column=0)
+
+app = BtcClient()
+# 设置窗口标题:
+app.master.title('BTC')
+app.master.geometry('200x100')
+# 主消息循环:
+app.mainloop()
+
 # print(u'现货行情')
 # print(okcoinSpot.ticker('btc_cny'))
 
@@ -129,16 +154,3 @@ if __name__ == '__main__':
 
 #print (u'期货逐仓持仓信息')
 #print (okcoinFuture.future_position_4fix('ltc_usd','this_week',1))
-
-# userData = okcoinSpot.userinfo()
-# print(userData)
-
-# userData = json.loads(userData)
-# if userData['result']:
-# 	info = userData['info']
-# 	funds = info['funds']
-# 	asset = funds['asset']
-# 	free = funds['free']
-# 	freezed = funds['freezed']
-# 	print('净资产：'+'%s'%asset['net']+'	总资产：'+'%s'%asset['total'])
-# 	print('btc：'+'%s'%free['btc']+'	RMB：'+'%s'%free['cny'])
